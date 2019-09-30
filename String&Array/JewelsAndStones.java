136067b26ec4c98a1c74aa0353eb6d9821a71a10
choiwono <treasure_b@naver.com>
2019-10-01 00:20:22 +0900

    보석과 돌..문제풀이

--- String&Array/JewelsAndStones.java
+++ String&Array/JewelsAndStones.java
@@ -1,12 +1,35 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class JewelsAndStones {
     public static void main(String[] args){
-
+        int answer = solution("aAce", "aAAbbbbAACCDDEEbaAwce");
+        System.out.println(answer);
     }
 
     public static int solution(String jewels, String stones){
         int answer = 0;
+        HashMap<String,Integer> map = new HashMap<>();
+        String[] stoneStr = stones.split("");
+        String[] jewlsStr = jewels.split("");
 
-        
+        for(int i=0; i<stoneStr.length; i++){
+            if(map.containsKey(stoneStr[i])){
+                map.put(stoneStr[i],map.get(stoneStr[i])+1);
+            } else {
+                map.put(stoneStr[i],1);
+            }
+        }
+        int i = 0;
+        for(String key : map.keySet()){
+            if(i == jewlsStr.length) return answer;
+            
+            if(key.equals(jewlsStr[i])){
+                answer += map.get(jewlsStr[i]);
+                i++;
+            }
+        }  
 
         return answer;        
     }
---
+++ String&Array/MeetingRoom2.java
@@ -0,0 +1,5 @@
+public class MeetingRoom2 {
+    public static void main(String[] args){
+        // 이건 내일.. 죽겠다 진짜
+    }
+}
