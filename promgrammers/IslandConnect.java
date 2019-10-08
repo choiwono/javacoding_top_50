6a3d0a069e72c270fba4929efdaaad8ec23fda18
choiwono <treasure_b@naver.com>
2019-10-08 23:59:31 +0900

    섬연결하기

--- promgrammers/IslandConnect.java
+++ promgrammers/IslandConnect.java
@@ -1,21 +1,33 @@
 public class IslandConnect {
-    public static void main(String[] args){
-        // n개의 섬 사이에 다리를 건설하는 비용(costs)이 주어질 때, 최소의 비용으로 모든 섬이 
-        // 서로 통행 가능하도록 만들 때 필요한 최소 비용을 return 하도록 solution을 완성하세요.
-        // 다리를 여러 번 건너더라도, 도달할 수만 있으면 통행 가능하다고 봅니다. 
-        // 예를 들어 A 섬과 B 섬 사이에 다리가 있고, B 섬과 C 섬 사이에 다리가 있으면 A 섬과 C 섬은 서로 통행 가능합니다.
-        int[][] costs = {{0,1,1},{0,2,2},{1,2,5},{1,3,1},{2,3,8}};
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        //Solution s = new Solution();
+        int[][] routes = { { -20, -15 }, { -14, -5 }, { -18, -13 }, { -5, -3 } };
+
+        int answer = solution(routes);
+        System.out.println(answer);
+
     }
 
-    public static int solution(int n, int[][] costs){
+    public static int solution(int[][] routes) {
         int answer = 0;
-        // 최소의 비용으로 모든섬이 통행이 가능한 상태로 만든다.
-        // 1. 알고리즘을 구하자..
-        // 2. 어느 자료구조를 활용할 것인지에 대해서 생각
-        // 3. for 문을 돌리자..
-        // 4. 제한 조건에 대해서 정리한다
 
-        
+        Arrays.sort(routes, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return Integer.compare(o1[1], o2[1]);
+                // 두 번째 항목을 기준으로 오름차순 정렬
+                // 첫 번째 항목으로 저장하려면 o1[0]과 o2[0]을 비교해 주면 된다.
+            }
+        });
+
+        int cameraPos = -30001;
+        for (int i = 0; i < routes.length; i++) {
+            if (cameraPos < routes[i][0]) {
+                answer++;
+                cameraPos = routes[i][1];
+            }
+        }
         return answer;
     }
 }
