d77fab95d05dfaa382e66d86e44fe285faa7ef8c
choiwono <treasure_b@naver.com>
2019-10-10 01:11:56 +0900

    targetNumber 풀이

--- promgrammers/targetNumber.java
+++ promgrammers/targetNumber.java
@@ -1,13 +1,29 @@
 public class targetNumber {
-    
-    public static void main(String[] args){
+    static int answer = 0;
 
+    public static void main(String[] args) {
+        int[] numbers = { 1, 1, 1, 1, 1 };
+        int target = 3;
+        solution(numbers, target);
     }
 
-    public int solution(int[] numbers, int target){
-       int answer = 0;
-       return answer; 
+    public static void dfs(int[] numbers, int index, int sum, int target) {
+
+        if (index > numbers.length)
+            return;
+        if (index == numbers.length) {
+            if (sum == target) {
+                answer++;
+            }
+            return;
+        }
+
+        dfs(numbers, index + 1, sum + numbers[index], target);
+        dfs(numbers, index - 1, sum + numbers[index], target);
     }
 
-    // dfs 문제풀이..
+    public static int solution(int[] numbers, int target) {
+        dfs(numbers, 0, 0, target);
+        return answer;
+    }
 }
